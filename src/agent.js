d9386aa72c9daa2acb11120582b3a391c1ce7a89
crazywebs <spatel2092@gmail.com>
2025-07-11 18:37:53 +0530

    first commit

--- src/agent.js
+++ src/agent.js
@@ -6,7 +6,7 @@ import { getUserDetail } from "./tools/userTool.js";
 const model = new ChatOpenAI({
   model: "gpt-4-turbo",
   temperature: 0,
-  apiKey: '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'
+  apiKey: ''
 });
 
 export const createAgent = async () => {
