ea4a6951e9e9440589975f81cd1f3b7bf63c3d89
crazywebs <spatel2092@gmail.com>
2025-07-14 10:44:10 +0530

    first commit

--- src/agent.js
+++ src/agent.js
@@ -2,11 +2,13 @@ import { createReactAgent } from "@langchain/langgraph/prebuilt";
 import { ChatOpenAI } from "@langchain/openai";
 import { convertCurrency } from "./tools/currencyTool.js";
 import { getUserDetail } from "./tools/userTool.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const model = new ChatOpenAI({
   model: "gpt-4-turbo",
   temperature: 0,
-  apiKey: ''
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 export const createAgent = async () => {
